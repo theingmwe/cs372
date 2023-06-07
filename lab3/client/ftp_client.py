67c1bc159f7c2d2c47350263478d09a3f495545f
theingmwe <theingmweoo.tmo@gmail.com>
2023-06-06 22:40:15 -0700

    progress

--- lab3/client/ftp_client.py
+++ lab3/client/ftp_client.py
@@ -1,2 +1,155 @@
 import os
 os.chdir('myfiles')
+
+import asyncio
+
+INTERFACE, SPORT = 'localhost', 8080
+
+async def receive_message(reader):
+    # First we receive the length of the message: this should be 8 total hexadecimal digits!
+    data_length_hex = await reader.readexactly(8)
+    data_length = int(data_length_hex.decode(), 16)  # Decode the received data before conversion
+
+    full_data = await reader.readexactly(data_length)
+    return full_data.decode()
+
+
+async def send_message(writer, message):
+    data_length_hex = hex(len(message))[2:].zfill(8)
+    writer.write(data_length_hex.encode())
+    writer.write(message.encode())
+    await writer.drain()
+
+
+async def list_files():
+    cwd = os.getcwd()  # Get the current working directory (cwd)
+    files = os.listdir(cwd)  # Get all the files in that directory    
+    return "\n".join(files)
+
+
+async def put_file(filename):
+    try:
+        with open(f"myfiles/{filename}", 'rb') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return None
+
+
+async def get_file(filename, content):
+    if content:
+        try:
+            with open(f"myfiles/{filename}", 'wb') as file:
+                file.write(content)
+                return True
+        except:
+            return False
+    else:
+        return False
+
+
+async def remove_file(filename):
+    try:
+        os.remove(f"myfiles/{filename}")
+        return True
+    except FileNotFoundError:
+        return False
+
+
+async def main():
+    reader, writer = await asyncio.open_connection(INTERFACE, SPORT)
+
+    # Receive password prompt
+    password_prompt = await receive_message(reader)
+    print(password_prompt)
+
+    while True:
+        # Enter password
+        password = input("> ")
+        await send_message(writer, password)
+
+        # Receive response
+        response = await receive_message(reader)
+        print(response)
+
+        # Check if login is successful
+        if response == "Login Successful!":
+            break
+
+    command_prompt = await receive_message(reader)
+    print(command_prompt)
+
+    # Continue with FTP functionality
+    while True:
+        # Receive command prompt
+        
+        # Enter command
+        command = input("> ")
+
+        await send_message(writer, command)
+
+        # Receive command response
+        response = await receive_message(reader)
+        # print(response)
+
+        # Process command
+        if command == "list":
+            if response == "ACK":
+                response = await receive_message(reader)
+                print(response)
+
+                # file_list = await list_files()
+                # await send_message(writer, file_list)
+                # print(file_list)
+            else:
+                print("Error: ", response)
+        elif command.startswith("put"):
+            if response == "ACK":
+                filename = command.split()[1]
+                content = await put_file(filename)
+                if content:
+                    await send_message(writer, "ACK")
+                    await send_message(writer, content)
+                else:
+                    await send_message(writer, "NAK File not found")
+            else:
+                print("Error:", response)
+        elif command.startswith("get"):
+            if response == "ACK":
+                filename = command.split()[1]
+                await send_message(writer, "ACK")
+                content = await receive_message(reader)
+                success = await get_file(filename, content)
+                if success:
+                    print("File downloaded successfully.")
+                else:
+                    print("Error: File not found on the server.")
+            else:
+                print("Error:", response)
+        elif command.startswith("remove"):
+            if response == "ACK":
+                filename = command.split()[1]
+                success = await remove_file(filename)
+                if success:
+                    print("File removed successfully.")
+                    await send_message(writer, "ACK")
+                else:
+                    await send_message(writer, "NAK File not found")
+            else:
+                print("Error:", response)
+        elif command == "close":
+            if response == "ACK":
+                writer.close()
+                await writer.wait_closed()
+                break
+            else:
+                print("Error:", response)
+        else:
+            print("Invalid command.")
+
+    print("Connection closed.")
+
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
--- lab3/server/ftp_server.py
+++ lab3/server/ftp_server.py
@@ -1,78 +1,121 @@
 import os
 os.chdir('myfiles')
 
-import socket
 import asyncio
 
 INTERFACE, SPORT = 'localhost', 8080
-CHUNK = 100
-PASSWORD = "password1"
-password_attempt = 0
-
-async def send_password_prompt(writer):
-    # When the client makes a connection to the server, the server should prompt it to enter a password.
-    # This password should be set ahead of time on the server.
-    password_message = "Please enter the password: "
-
-    writer.write(password_message.encode())
-    await writer.drain()
+MAX_ATTEMPTS = 3
 
 
-async def send_intro_message(writer):
-    intro_message = "Hello! Welcome to my (oot, oommenr, campbel2) server! I'm majoring in CS\n"
+async def handle_client(reader, writer):
+    password = "password123"  # Set the expected password here
+    attempts = 0
 
-    writer.write(intro_message.encode())
-    await writer.drain()
+    # Send password prompt
+    await send_message(writer, "Please enter the password: ")
 
-    
-async def receive_long_message(reader: asyncio.StreamReader):
+    while attempts < MAX_ATTEMPTS:
+        # Receive password from the client
+        received_password = await receive_message(reader)
+
+        # Check if the password is correct
+        if received_password == password:
+            await send_message(writer, "Login Successful!")
+            break
+        else:
+            attempts += 1
+            if attempts < MAX_ATTEMPTS:
+                await send_message(writer, f"Invalid password. Please try again ({MAX_ATTEMPTS - attempts} attempt(s) remaining): ")
+            else:
+                await send_message(writer, "Maximum attempts reached. Closing the connection.")
+                break
+
+    # Continue with FTP functionality if the login is successful
+    if attempts < MAX_ATTEMPTS:
+        await send_message(writer, "Welcome to the FTP server!")
+
+        while True:
+            # Receive command from the client
+            command = await receive_message(reader)
+
+            # Process the command
+            if command == "list":
+                cwd = os.getcwd()  # Get the current working directory (cwd)
+                files = os.listdir(cwd)  # Get all the files in that directory
+                # print("Files in %r: %s" % (cwd, files))
+                file_list = "\n".join(files)
+                await send_message(writer, "ACK")
+                await send_message(writer, file_list)
+                
+            elif command.startswith("put"):
+                _, filename = command.split(" ")
+                await receive_file(reader, filename)
+                await send_message(writer, f"File '{filename}' uploaded successfully.")
+            elif command.startswith("get"):
+                _, filename = command.split(" ")
+                await send_file(writer, filename)
+            elif command.startswith("remove"):
+                _, filename = command.split(" ")
+                try:
+                    os.remove(os.path.join("myfiles", filename))
+                    await send_message(writer, f"File '{filename}' removed successfully.")
+                except FileNotFoundError:
+                    await send_message(writer, f"File '{filename}' not found.")
+            elif command == "close":
+                break
+            else:
+                await send_message(writer, f"Invalid command: {command}")
+
+    writer.close()
+    await writer.wait_closed()
+
+
+async def receive_message(reader):
     # First we receive the length of the message: this should be 8 total hexadecimal digits!
-    # Note: `socket.MSG_WAITALL` is just to make sure the data is received in this case.
     data_length_hex = await reader.readexactly(8)
-
-    # Then we convert it from hex to integer format that we can work with
-    data_length = int(data_length_hex, 16)
+    data_length = int(data_length_hex.decode(), 16)  # Decode the received data before conversion
 
     full_data = await reader.readexactly(data_length)
     return full_data.decode()
 
 
-async def handle_client(reader, writer):
-    #prompt user for password
-    await send_password_prompt(writer)
+async def send_message(writer, message):
+    data_length_hex = hex(len(message))[2:].zfill(8)
+    writer.write(data_length_hex.encode())
+    writer.write(message.encode())
+    await writer.drain()
 
-    # receive password
-    message = await receive_long_message(reader)
-    writer.close()
-    await writer.wait_closed()
 
-    #check if password is correct or not
-    if message.strip() == PASSWORD:
-        #if correct, continue as lab2
-        print("Login Successful!")
+async def receive_file(reader, filename):
+    file_path = os.path.join("myfiles", filename)
+    with open(file_path, "wb") as file:
+        # Receive the file data in chunks and write to the file
+        while True:
+            data_chunk = await receive_message(reader)
+            if not data_chunk:
+                break
+            file.write(data_chunk.encode())
 
-        await send_intro_message(writer)
-        message = await receive_long_message(reader)
 
-        print("done: " +  message[-8:])
-
-        writer.close()
-        await writer.wait_closed()
-
-    else:
-        password_attempt += 1
-        
-        #TODO: if password_attempt = 3, implement error handling
+async def send_file(writer, filename):
+    file_path = os.path.join("myfiles", filename)
+    try:
+        with open(file_path, "rb") as file:
+            file_data = file.read()
+            await send_message(writer, file_data)
+    except FileNotFoundError:
+        await send_message(writer, f"File '{filename}' not found.")
 
 
 async def main():
-    server = await asyncio.start_server(
-            handle_client,
-            INTERFACE, SPORT
-    )
+    server = await asyncio.start_server(handle_client, INTERFACE, SPORT)
+
+    addr = server.sockets[0].getsockname()
+    print(f'Serving on {addr}')
 
     async with server:
         await server.serve_forever()
+
 
 # Run the `main()` function
 if __name__ == "__main__":
